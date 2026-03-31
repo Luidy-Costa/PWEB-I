4a472d78528f78aa309e7a3a3414a85559063f2d
Luidy Costa <lluidycosta2024@gmail.com>
2026-03-31 16:58:18 -0300

    feat: resolve questão 11 com array multidimensional

---
+++ Praticas Laboratoriais/Pratica 1/questão-11/questão-11.php
@@ -0,0 +1,18 @@
+<?php
+$boletim = [
+    "Luidy" => [
+        "PHP" => 9.5,
+        "Python" => 10.0,
+        "Redes" => 8.5
+    ],
+    "Maria" => [
+        "PHP" => 8.0,
+        "Python" => 7.5,
+        "Redes" => 9.0
+    ],
+    "João" => [
+        "PHP" => 6.5,
+        "Python" => 8.0,
+        "Redes" => 7.0
+    ]
+];

0cece54260606f7d06afcd5603e3499f91d4729b
Luidy Costa <lluidycosta2024@gmail.com>
2026-04-01 15:18:41 -0300

    feat: resolve questão 20

---
+++ Praticas Laboratoriais/Pratica 1/questão-20/questão-20.php
@@ -0,0 +1,10 @@
+<?php
+$nome = "Luidy";
+$email = "luidy@rpegas.com.br";
+$idade = 22;
+
+$mensagem_cadastro = "Sucesso! O usuário $nome foi cadastrado com o e-mail $email e tem $idade anos.";
+
+echo "<h3>Confirmação de Sistema</h3>";
+echo $mensagem_cadastro;
+?>
